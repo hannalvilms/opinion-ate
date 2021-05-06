0ca6bf8c61fa88e03a71e58b8f596a2773a71aaf
hanna-liisavilms <hannaliisavilms@gmail.com>
2021-05-06 21:55:42 +0300

    Show Loading and Error States

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "https://outside-in-dev-api.herokuapp.com/uUyXHOZw0n1pLp74uyZwBaKfnLoFsNxL",
+  baseURL:
+    "https://outside-in-dev-api.herokuapp.com/uUyXHOZw0n1pLp74uyZwBaKfnLoFsNxL",
 });
 
 const api = {
--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,17 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, Fragment } from "react";
 import { connect } from "react-redux";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
+import Alert from "@material-ui/lab/Alert";
 import { loadRestaurants } from "../store/restaurants/actions";
 
-export const RestaurantList = ({ loadRestaurants, restaurants }) => {
+export const RestaurantList = ({
+  loadRestaurants,
+  restaurants,
+  loading,
+  loadError,
+}) => {
   useEffect(() => {
     loadRestaurants();
   }, [loadRestaurants]);
 
   return (
     <List>
+      {loading && <CircularProgress data-testid="loading-indicator" />}
+      {loadError && (
+        <Alert severity="error">Restaurants could not be loaded.</Alert>
+      )}
       {restaurants.map((restaurant) => (
         <ListItem key={restaurant.id}>
           <ListItemText>{restaurant.name}</ListItemText>
@@ -23,6 +34,8 @@ export const RestaurantList = ({ loadRestaurants, restaurants }) => {
 
 const mapStateToProps = (state) => ({
   restaurants: state.restaurants.records,
+  loading: state.restaurants.loading,
+  loadError: state.restaurants.loadError,
 });
 
 const mapDispatchToProps = { loadRestaurants };
